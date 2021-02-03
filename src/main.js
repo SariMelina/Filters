467b7916246424247c80816504cecd42bb51e990
SariMelina <mmkings_beba@hotmail.com>
2021-02-03 12:53:50 -0600

    global filter added

--- src/main.js
+++ src/main.js
@@ -1,12 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
-import router from './router'
-import store from './store'
 
 Vue.config.productionTip = false
 
+Vue.filter('suspensivos', function(texto){
+  return texto.substring(0, 10) + ' ...'
+})
+
 new Vue({
-  router,
-  store,
   render: h => h(App)
 }).$mount('#app')
